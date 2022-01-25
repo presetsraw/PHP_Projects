8bdbe194473e5bb5783a0705a0ddada2d38e2ac3
Saksham Sharma <sakshamsharma150998@gmail.com>
2022-01-25 16:20:58 +0530

    Delete MusicoConnect directory

--- MusicoConnect/session/denied.php
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-  <p style="font-size:36px">Access Denied! :P </p>
-  <p class="style1">Please <a href="index.php">Log In </a>to continue... :) </p>
-</body>
-</html>
--- MusicoConnect/system/application/views/view_new_message.php
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript">
-$(document).ready(function() {
-    $("#newmessage").html('<img src="<?php echo MAINSITE_URL;?>img/spacer.gif" width="5" height="17" /><img src="<?php echo MAINSITE_URL;?>img/envelope.gif" />');
-});
-</script>
