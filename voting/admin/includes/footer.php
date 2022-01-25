5dd32e563f7bfba46c8db40791f4177b7dd12729
Saksham Sharma <sakshamsharma150998@gmail.com>
2022-01-25 16:22:19 +0530

    Delete voting directory

--- voting/admin/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Voting System</b>
-    </div>
-    <strong> phptraining.com</a></strong>
-</footer>
--- voting/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-    <div class="container">
-      <div class="pull-right hidden-xs">
-        <b>Voting System</b>
-      </div>
-      <strong> <a href="https://code-projects.org/"> www.phptraining.com</a></strong>
-    </div>
-    <!-- /.container -->
-</footer>
